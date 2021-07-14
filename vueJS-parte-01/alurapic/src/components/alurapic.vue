dff116167225bc838c9a7cbcf933d09f1cbff29f
TeixeiraCamila <teixeira0101camila@gmail.com>
2021-07-14 20:22:32 -0300

    Primeira api request

---
+++ vueJS-parte-01/alurapic/src/components/alurapic.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h1>{{ tituloPage }}</h1>
+    <ul v-for=" (foto, i) in fotos" :key="i">
+      <li> <img :src="foto.url" :alt="foto.titulo"/> </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+
+  data(){
+   return{
+      tituloPage: 'Alurapic',
+      fotos: []
+      
+   }
+  },
+  created(){
+    this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())
+      .then(fotos => this.fotos = fotos);
+  }
+  
+}
+</script>
+
+<style scoped>
+
+</style>
